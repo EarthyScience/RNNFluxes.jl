d1699b01025549dd8fb309a1cf4ff1faa1bbf655
mreichstein <mreichstein@bgc-jena.mpg.de>
2016-11-21 14:31:48 +0100

    Just some updates on the plotSummary fro better interpretation

--- src/plotProgress.jl
+++ src/plotProgress.jl
@@ -10,7 +10,23 @@ function plotSummary(model)
       gr()
       yNorm=model.yNorm
       pred=predict(model,model.weights,model.xNorm)
-      display(plot([model.lossesTrain, model.lossesVali],color=["blue" "orange"],fmt=:png))
-      display(plot(vec(pred), vec(yNorm), line=:scatter, color=:black, markeralpha=0.05,fmt=:png))
+	  #Plot whole training trace
+      display(plot([model.lossesTrain, model.lossesVali],color=["blue" "orange"],fmt=:png, label=["Training" "Validation"]))
+	  #Plot only last 80% and no peaks
+	  nEpoch=length(model.lossesTrain)
+	  start=trunc(Int,nEpoch*0.2)
+	  idx=start:nEpoch
+	  upperQuantTrainLoss=quantile(model.lossesTrain[idx], 0.95)
+	  upperQuantValiLoss=quantile(model.lossesVali[idx], 0.95)
+
+	  miniTrainLoss=minimum(model.lossesTrain[idx])
+	  miniValiLoss=minimum(model.lossesVali[idx])
+      display(plot([idx,idx],[model.lossesTrain[idx], model.lossesVali[idx]],color=["blue" "orange"], fmt=:png, ylim=[(miniTrainLoss, upperQuantTrainLoss) (miniValiLoss,upperQuantValiLoss)], label=["Training" "Validation"], layout=(2,1)))
+
+      #Scatterplot MOD vs OBS
+	  min=minimum(vec(yNorm))
+	  max=maximum(vec(yNorm))
+	  
+      display(plot([vec(pred), [min, max]], [vec(yNorm), [min, max]], line=[:scatter :line], color=[:black :red], markeralpha=0.05, label=["OBS vs. MOD", "1:1-line"], fmt=:png))
       println("Correlation: ", cor(vec(pred), vec(yNorm)))
 end
