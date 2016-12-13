e80a66cda7f00c6bf79f2446c396b6598bcb5ccc
nhuckle <nhuckle@pc026.bgc-jena.mpg.de>
2016-12-13 18:03:10 +0100

    more work on the explicit gradient

--- src/rnn.jl
+++ src/rnn.jl
@@ -94,7 +94,83 @@ end
 derivActivation(dh) = dot(sigm(dh),(1-sigm(dh))) # Maybe this should be matrix mult of dh * dh'
 derivloss(ytrue,ypred)=ytrue-ypred
 
-function predict_with_gradient(::RNNModel,w, x,ytrue) ### This implements w as a vector
+function predict_with_gradient_new(::RNNModel,w, x,ytrue) ### This implements w as a vector
+
+  nTimes, nSamp, nVar = size(x)
+  nHid = Int(-0.5*(nVar + 2) + sqrt(0.25*(nVar+2)^2-1+length(w)))
+
+  ypred=Array{typeof(w[1])}(nTimes, nSamp)
+  w1=reshape(w[1:nVar*nHid], nVar, nHid)
+  w2=reshape(w[nVar*nHid+1:nVar*nHid+nHid*nHid], nHid, nHid)
+  w3=reshape(w[nVar*nHid+nHid*nHid+1:nVar*nHid+nHid*nHid+nHid], nHid, 1)
+  w4=reshape(w[nVar*nHid+nHid*nHid+nHid+1:nVar*nHid+nHid*nHid+nHid+nHid], 1, nHid)
+  w5=reshape(w[nVar*nHid+nHid*nHid+nHid+nHid+1:nVar*nHid+nHid*nHid+nHid+nHid+1], 1)
+  
+    
+  dWxh, dWhh, dWhy = [zeros(size(w1)) for i=1:nSamp], [zeros(size(w2)) for i=1:nSamp], [zeros(size(w3)) for i=1:nSamp]
+  dbh, dby = [zeros(size(w4)) for i=1:nSamp], [zeros(size(w5)) for i=1:nSamp]
+  dhnext = 0
+
+  for s=1:nSamp  ## could this loop be parallelized into say 100 parallel instances? Even a speed up factor 10 would make quite some diff
+
+    hidden = zeros(eltype(w[1]),1, nHid,nTimes+1)
+    dhnext = 0
+        loss = 0
+    for i=1:nTimes
+      ## w[1] weights from input to hidden
+      ## w[2] weights hidden to hidden
+      ## w[3] weights hidden to output
+      ## w[4] bias to hidden
+      ## w[5] bias to output
+      #hidden[:,:,i] = sigm(x[i:i, s, :] * w1 + w4 + hidden[:,:,max(i,1)] * w2)
+      hidden[:,:,i] = sigm(x[i:i, s, :] * w1 + w4 + hidden[:,:,max(i-1,1)] * w2)
+      # hidden[:,:,i] = tanh(x[i:i, s, :] * w1 + w4 + hidden[:,:,max(i,1)] * w2)
+      # ypred[i,s]=sigm(hidden * w3)[1] + w5[1]
+      #ypred[i,s]=sigm(hidden[:,:,i] * w3 + w5)[1]
+      ypred[i,s] = sigm(hidden[:,:,i] * w3 + w5)[1]
+      #loss += sqrt(sum((ypred[i,s] - ytrue[i,s])^2))
+    end
+
+    for i=nTimes:-1:1
+      ## w[1] weights from input to hidden
+      ## w[2] weights hidden to hidden
+      ## w[3] weights hidden to output
+      ## w[4] bias to hidden
+      ## w[5] bias to output
+      dy = ytrue[i,s] - ypred[i,s]
+      dWhy[s] += hidden[:,:,i]' * dy # must turn it around because t(dWhy)
+            dWhy[s] += hidden[:,:,i]' * dy * ypred[i,s] * ypred[i,s]
+      dby[s] += dy * ypred[i,s] * ypred[i,s]
+      dh = w3 * dy * ypred[i,s] * ypred[i,s] + dhnext' #no t(w3) because it already is
+      ## dhraw = derivActivation(hidden[:,:,i])*dh
+      dhraw = (hidden[:,:,i]') .* (1 - (hidden[:,:,i]')) .* dh
+      #dhraw = (1 - hidden[:,:,i]' .* hidden[:,:,i]') .*dh
+      dbh[s]  += dhraw'
+      #dWxh[s] += (dhraw * x[i:i,s,:])'
+      dWxh[s] += x[i:i,s,:]' * dhraw' 
+      #dWhh[s] += dhraw * (i==1 ? zeros(1,nHid) : hidden[:,:,i-1])
+      dWhh[s] += (i==1 ? zeros(1,nHid)' : hidden[:,:,i-1]') * dhraw'
+      dhnext = dhraw' * w2
+      # ypred[i,s]=sigm(hidden * w3)[1] + w5[1
+      if s==1
+        #println(i)
+        #println(dWhh[s])
+      end
+    end
+            #dWhh = clamp(dWhh, -10, 10)
+            #dWxh = clamp(dWxh, -10, 10)
+            #dby = clamp(dby, -10,10)
+            #dbh = clamp(dbh, -10,10)
+            #dWhy = clamp(dWhy, -10,10)
+  end
+
+  #return ypred, dWxh, dWhh, dWhy, dbh, dby
+  #return [dWxh ; dWhh ; dWhy ; dbh ; dby]
+  #return [reshape(reduce(+,dWxh), nVar*nHid ); reshape(reduce(+,dWhh), nHid*nHid) ; reshape(reduce(+,dWhy),nHid) ; reshape(reduce(+,dbh),nHid) ; reshape(reduce(+,dby),1)]
+  return [reshape(dWxh[5], nVar*nHid ); reshape(dWhh[5], nHid*nHid) ; reshape(dWhy[5],nHid) ; reshape(dbh[5],nHid) ; reshape(dby[5],1)]
+end
+
+function predict_with_gradient_old(::RNNModel,w, x,ytrue) ### This implements w as a vector
 
   nTimes, nSamp, nVar = size(x)
   nHid = Int(-0.5*(nVar + 2) + sqrt(0.25*(nVar+2)^2-1+length(w)))
@@ -436,3 +512,141 @@ function raggedToVector(raggedArray)
     end
     return ragged_as_Vector
 end
+
+
+#Training:
+## Input x[nTimes, nSamples, nVar]
+##       y[nTimes, nSamples]
+##       nEpochs
+function train_net_gradient(
+    model::FluxModel,
+    # Predictors, target and number of Epochs (y is still 1 variable only, can be changed to multioutput, but for us I don't see an urgent application)
+    x, y, nEpoch=201;
+    ## defines how a prediction is turned into a loss-Value (e.g. sum of squares, sum of abs, weighted squares etc..)
+    lossFunc=mseLoss,
+    ## How many samples are used in each epoch
+    batchSize=1,
+    ## Define search algorithm including parameters
+    searchParams = Adam(model.weights; lr=0.01, beta1=0.9, beta2=0.95, t=1, eps=1e-6, fstm=zeros(model.weights), scndm=zeros(model.weights)),
+    ### How often will be intermediate output given
+    infoStepSize=100,
+    ### Also graphical output via the react/interact interface?
+    plotProgress=false
+    )
+
+    nTimes, nSamp, nVarX=size(x)
+
+    lossesTrain = model.lossesTrain
+    lossesVali = model.lossesVali
+
+    ## Define the used loss-Function based on the method to predict and the function defining how the predictions
+    ## are compared with the data (default is Mean Squared Error across sequences)
+    loss(w, x, y)=lossFunc(w, x, y, model)
+    lossgradient=grad(loss)
+
+    ### Normalize y to 0,1
+    yMin, yMax = extrema(y)
+    yNorm=(y-yMin)/(yMax-yMin)
+    ### Normalize x to -1, 1 per Variable (not necessarily the best way to do it....)
+    xEx=vec(extrema(x, [1,2]))
+    xNorm=copy(x)
+    for v in 1:size(xEx,1);
+        xNorm[:,:,v] = 2.0.* ((x[:,:,v]-xEx[v][1])/(xEx[v][2]-xEx[v][1])-0.5)
+    end
+    model.yNorm, model.xNorm = yNorm, xNorm
+    model.yMin, model.yMax, model.xEx = yMin, yMax, xEx
+
+    ## Split data set for cross-validation
+    ## Could be parameterized in the function call later!!
+    ## Also missing a full k-fold crossvalidation
+    valiFrac=0.2
+    permut=shuffle(1:nSamp)
+
+    valiNSamp = trunc(Int, valiFrac*nSamp)
+
+    if valiNSamp >= 2
+        valiIdx=permut[1:valiNSamp]
+        trainIdx=permut[valiNSamp+1:end]
+    else
+        valiIdx=1:nSamp
+        trainIdx=1:nSamp
+        warn("Too few samples (only $valiNSamp) for creating validation set!! Full data set (n=$nSamp sequences) used for training...")
+    end
+    if length(trainIdx) < batchSize
+        warn("Too few samples for batchSize $(batchSize)! BatchSize adjusted to full training size $(length(trainIdx))")
+        batchSize=length(trainIdx)
+
+    end
+
+
+    #valiIdx=1:nSamp; trainIdx=1:nSamp
+    #display(plot(layer(x = vec(xNorm)[1:1000], y = vec(x)[1:1000], Geom.point)))
+
+
+    w = model.weights
+
+    info("Starting training....")
+    info( length(trainIdx), " Training samples, ",  length(valiIdx), " Validation samples. " )
+    info("BatchSize: " , batchSize)
+
+    ### Loss before training
+    lossesPreTrain=loss(w, xNorm[:, trainIdx,:], yNorm[:, trainIdx, :])
+    lossesPreVali=loss(w, xNorm[:, valiIdx,:], yNorm[:, valiIdx, :])
+    info("Before training loss, Training set: ", lossesPreTrain, " Validation: ", lossesPreVali)
+
+    irsig=model.interruptSignal
+
+    ### Just for plotting performace select not too many points (otherwise my notebook freezes etc)
+    if plotProgress
+
+      cb=checkbox(label="Interrupt",signal=irsig)
+      display(cb)
+      plotSample = sample(1:length(yNorm[:, trainIdx, :]), min(1000,length(yNorm[:, trainIdx, :])) , replace=false)
+      yvals=Signal((rand(200), rand(200), rand(1000).*10, rand(1000)./10))
+      display(map(plotSignal, yvals))
+
+    end
+
+
+    ### Here would be could if one could stop the loop based on user input to a checkbox (does not work)
+    ### (Fabian knows what mean)
+   # b=checkbox()
+   # display(b)
+   # quitLoop=Signal(b.value)
+
+    for i=1:nEpoch
+
+        value(irsig) && break
+        ### Batch approach: cylce randomly through subset of samples (==> more stochasticitym better performance (in theory))
+        ### I don't understand why the performance does not get much better with smaller batches
+        nTrSamp = length(trainIdx)
+        batchIdx=sample(1:nTrSamp,batchSize, replace=false)
+        ### Calc the loss gradient dloss/dw based on the current weight vector and the sample
+        ### This is done here with the predef Adagrad method. Could be done explicitely to speed up
+        #dw = lossgradient(w, xNorm[:,trainIdx[batchIdx] ,:], yNorm[:, trainIdx[batchIdx],:])
+		dw = predict_with_gradient_new(model, w, xNorm[:,trainIdx[batchIdx] ,:], yNorm[:, trainIdx[batchIdx],:])
+		#println(size(dw))
+        ### Update w according to loss gradient and algorithm (incl, parameters therein)
+        w, params = update!(w, dw, searchParams)
+
+        ### Loss on training set and on validation set
+        ### Early stopping based on the validation set could be implemented (when validation loss gets worse again)
+        ### but it will be heuristic, because one has to smooth the loss series (with batch there is noise)
+        push!(lossesTrain,loss(w, xNorm[:, trainIdx,:], yNorm[:, trainIdx, :]))
+        push!(lossesVali,loss(w, xNorm[:, valiIdx,:], yNorm[:, valiIdx, :]))
+
+        ### Output
+        if rem(i, infoStepSize) == 1
+            println("Epoch $i, Training: ", lossesTrain[i], " Validation: ", lossesVali[i])
+            ## For graphical real time monitoring (see cell above)
+            curPredAll=predict(model,w, xNorm)
+            #println(typeof(yNorm))
+            if plotProgress
+              newData=(lossesTrain, lossesVali,vec(curPredAll[:, trainIdx,:])[plotSample], vec(yNorm[:,trainIdx,:])[plotSample])
+              plotProgress && push!(yvals, newData)
+            end
+        end
+    end
+    return model
+end
+
