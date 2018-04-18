348a842b39f79db673e5d1c3a29f876d9485bec3
autodocs <autodocs>
2018-04-18 07:36:19 +0000

    build based on 04a14bc

--- latest/search_index.js
+++ latest/search_index.js
@@ -61,7 +61,7 @@ var documenterSearchIndex = {"docs": [
     "page": "A quick Overview",
     "title": "A quick Overview",
     "category": "section",
-    "text": "Here is a quick demonstration of how to train and apply an LSTM. Lets create some artificial input data first:nTime=100\nnSample=200\nnVarX = 3\nx = [rand(nVarX,nTime) for i=1:nSample]\n\ntrue_model(x) = transpose(cumsum(x[1,:])+exp.(x[2,:])+cumprod(1.7*x[3,:]))\n\ny = true_model.(x);Let's see what we have here. x contains the predictor variables. It is contains 200 samples of multivariate time series. Each sample is a nVar x nTime matrix containing the inputs for the model. Our function true_model generates a new time series from such a multivariate time series and is applied to every input sample. So our target variable y contains nSample row Vectors of length ntime.Now we create a model:using RNNFluxes\nnHid=15\nm=RNNFluxes.LSTMModel(nVarX,nHid,nDropout=1)This initializes an LSTM model containing 15 hidden nodes which represent the persistent state of the model. To train the model we can run:train_net(m,x,y,5001);This trains the model m for 5001 update steps. Note that the model variable m is updated in-place. To test how well our model performs, we generate a test dataset:xtest = [rand(nVarX,nTime)]\nytest = true_model.(xtest)[1]and do a prediction using our trained model mypred = predict_after_train(m,xtest)[1]Now we can compare the true vs predicted results:using Plots\ngr()\nplot(ytest',ypred)"
+    "text": "Here is a quick demonstration of how to train and apply an LSTM. Lets create some artificial input data first:nTime=100\nnSample=200\nnVarX = 3\nx = [rand(nVarX,nTime) for i=1:nSample]\n\ntrue_model(x) = transpose(cumsum(x[1,:])+exp.(x[2,:])+cumprod(1.7*x[3,:]))\n\ny = true_model.(x);Let\'s see what we have here. x contains the predictor variables. It is contains 200 samples of multivariate time series. Each sample is a nVar x nTime matrix containing the inputs for the model. Our function true_model generates a new time series from such a multivariate time series and is applied to every input sample. So our target variable y contains nSample row Vectors of length ntime.Now we create a model:using RNNFluxes\nnHid=15\nm=RNNFluxes.LSTMModel(nVarX,nHid,nDropout=1)This initializes an LSTM model containing 15 hidden nodes which represent the persistent state of the model. To train the model we can run:train_net(m,x,y,5001);This trains the model m for 5001 update steps. Note that the model variable m is updated in-place. To test how well our model performs, we generate a test dataset:xtest = [rand(nVarX,nTime)]\nytest = true_model.(xtest)[1]and do a prediction using our trained model mypred = predict_after_train(m,xtest)[1]Now we can compare the true vs predicted results:using Plots\ngr()\nplot(ytest\',ypred)"
 },
 
 {
@@ -84,7 +84,7 @@ var documenterSearchIndex = {"docs": [
     "location": "models.html#RNNFluxes.LSTMModel",
     "page": "Available Models",
     "title": "RNNFluxes.LSTMModel",
-    "category": "Type",
+    "category": "type",
     "text": "type LSTMModel\n\nImplementation of an LSTM with handcoded gradients. It has the following constructor:\n\nLSTMModel(nVar,nHid; dist = Uniform, forgetBias = 1, nDropout=nHid ÷ 10)\n\nParameters\n\nnVar number of input (predictor) variables for each time step\nnHid number of hidden nodes contained in the LSTM\ndist distribution to generate initial Weights, currently Uniform and Normal are supported, defaults to Uniform\nforgetBias determine bias of generating forget bias weights, defaults to 1\nnDropOut number of dropout nodes for each prediction, defaults to 10% of the nodes\n\n\n\n"
 },
 
@@ -92,7 +92,7 @@ var documenterSearchIndex = {"docs": [
     "location": "models.html#RNNFluxes.RNNModel",
     "page": "Available Models",
     "title": "RNNFluxes.RNNModel",
-    "category": "Type",
+    "category": "type",
     "text": "type RNN\n\nImplementation of an RNN with handcoded gradients.\n\nRNNModel(nVar,nHid;dist=Uniform)\n\nParameters\n\nnVar number of input (predictor) variables for each time step\nnHid number of hidden nodes contained in the RNN\ndist distribution to generate initial Weights, currently Uniform and Normal are supported, defaults to Uniform\n\n\n\n"
 },
 
@@ -108,7 +108,7 @@ var documenterSearchIndex = {"docs": [
     "location": "models.html#RNNFluxes.GenericModel",
     "page": "Available Models",
     "title": "RNNFluxes.GenericModel",
-    "category": "Type",
+    "category": "type",
     "text": "type GenericModel\n\nType wrapping a generic model that is differentiated with AutoGrad. Only a predict function is needed.\n\n\n\n"
 },
 
@@ -117,7 +117,7 @@ var documenterSearchIndex = {"docs": [
     "page": "Available Models",
     "title": "Trainning a generic model using AutoDiff",
     "category": "section",
-    "text": "We have also wrapped the training and plotting machinery here together with the ForwardDiff package, so that you can invert any custom model, too. Please note that, for this prupose the package (Flux.jl)[https://github.com/FluxML/Flux.jl] might be a better and well-maintained option.  RNNFluxes.GenericModelA short example on how to define and train such a generic model follows. Let's assume we already know the model structure but want to estimate the parameters. Any Julia code would be valid here in defining this model:function my_generic_model{T}(model,w::AbstractArray{T},x)\n\n    a = w[1]\n    b = w[2]\n    c = w[3]\n\n    map(x) do ix\n        cumsum(a * ix[1,:])+exp.(b * ix[2,:])+cumprod(c * ix[3,:])\n    end\nendWe generate some artificial data again:nTime=100\nnSample=200\nnVarX = 3\nx = [rand(nVarX,nTime) for i=1:nSample]\n\ntrue_model(x) = transpose(cumsum(x[1,:])+exp.(x[2,:])+cumprod(1.7*x[3,:]))\n\ny = true_model.(x);In the next step we define a GenericModel which includes our above-mentioned predict function.params = ()\nw      = rand(3) #Init weights\nm      = RNNFluxes.GenericModel(params,w,my_ANN_model)Training works \"as usual\"t = train_net(m,x,y,4001);"
+    "text": "We have also wrapped the training and plotting machinery here together with the ForwardDiff package, so that you can invert any custom model, too. Please note that, for this prupose the package (Flux.jl)[https://github.com/FluxML/Flux.jl] might be a better and well-maintained option.  RNNFluxes.GenericModelA short example on how to define and train such a generic model follows. Let\'s assume we already know the model structure but want to estimate the parameters. Any Julia code would be valid here in defining this model:function my_generic_model{T}(model,w::AbstractArray{T},x)\n\n    a = w[1]\n    b = w[2]\n    c = w[3]\n\n    map(x) do ix\n        cumsum(a * ix[1,:])+exp.(b * ix[2,:])+cumprod(c * ix[3,:])\n    end\nendWe generate some artificial data again:nTime=100\nnSample=200\nnVarX = 3\nx = [rand(nVarX,nTime) for i=1:nSample]\n\ntrue_model(x) = transpose(cumsum(x[1,:])+exp.(x[2,:])+cumprod(1.7*x[3,:]))\n\ny = true_model.(x);In the next step we define a GenericModel which includes our above-mentioned predict function.params = ()\nw      = rand(3) #Init weights\nm      = RNNFluxes.GenericModel(params,w,my_ANN_model)Training works \"as usual\"t = train_net(m,x,y,4001);"
 },
 
 {
@@ -132,7 +132,7 @@ var documenterSearchIndex = {"docs": [
     "location": "training.html#RNNFluxes.train_net",
     "page": "Training and predicting models",
     "title": "RNNFluxes.train_net",
-    "category": "Function",
+    "category": "function",
     "text": "train_net(model::FluxModel, x, y, nEpoch)\n\nTrains a model defined in model on the given predictors x and labels y for nEpoch training steps. Here, x as well as y are vectors of matrices of size (nVar x nTimeStep). Each matrix stands for a single sample, for example a tower time series. The length of the individual time series is allowed to differ.\n\nKeyword arguments\n\nlossFunc lossFUnction to be evaluated, defaults to mseLoss\nbatchSize number of samples to be used for each update step\nsearchParams One of the training methods defined in Knet. Can be any object of type Sgd, Momentum, Adam, Adagrad, Adadelta, Rmsprop. Have a look at update.jl to see how to construct this\ninfoStepSize stepsize in which traing and validation losses will be calculated and printed to the screen\nvaliFrac fraction of samples to be omitted from training as a validation dataset. Can be either a Float for a random fraction or vector of indices of omitted datasets, defaults to 0.2\nlosscalcsize determines how often the losses of the whole dataset will be evaluated so that the training process can be analyzed afterwards\nplotProgress shall the training be visualized, works only in Jupyter notebooks\nnPlotsample number of sample points used for plotting\n\n\n\n"
 },
 
@@ -140,7 +140,7 @@ var documenterSearchIndex = {"docs": [
     "location": "training.html#RNNFluxes.predict_after_train",
     "page": "Training and predicting models",
     "title": "RNNFluxes.predict_after_train",
-    "category": "Function",
+    "category": "function",
     "text": "Given the trained model this function does predictions on another data set it also does the back-transform of the predicted values from (0,1) to original range whole thing should be improved. An object of Type TrainedModel should be used or sth like a list in R\n\n\n\n"
 },
 
